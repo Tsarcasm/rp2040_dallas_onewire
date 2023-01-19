53921f5ac6d0d03110ca46c9d9f81c05d7defafd
tsarcasm <lockiecharles@gmail.com>
2023-01-19 17:20:29 +0000

    Added simple blink.pio

--- dallas_onewire.c
+++ dallas_onewire.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#include "blink.pio.h"
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
 
 int main() {
@@ -10,13 +13,21 @@ int main() {
 
     printf("Hello, Raspberry Pi Pico!\n");
 
-    // blink onboard led
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    static const uint led_pin = PICO_DEFAULT_LED_PIN;
+    static const float pio_freq = 2000;  // 2kHz
+
+    PIO pio = pio0;
+    uint sm = pio_claim_unused_sm(pio, true);
+    uint offset = pio_add_program(pio, &blink_program);
+    // get clock divider
+    uint div = clock_get_hz(clk_sys) / pio_freq;
+    // initialize program
+    blink_program_init(pio, sm, offset, led_pin, div);
+    // start program
+    pio_sm_set_enabled(pio, sm, true);
+
+    // do nothing
     while (true) {
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(500);
+        sleep_ms(1000);
     }
 }
